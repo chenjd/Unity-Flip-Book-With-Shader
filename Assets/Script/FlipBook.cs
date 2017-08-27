a69a761430e68e89380946f58b65994bc8af123c
Jiadong Chen <Jiadong Chen>
2017-08-27 17:55:33 +0800

    完成第一个pass，渲染第一页的效果

---
+++ Assets/Script/FlipBook.cs
@@ -0,0 +1,86 @@
+﻿//
+// created by jiadong chen
+// http://www.chenjd.me
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FlipBook : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+
+    #region 字段
+
+    public List<Texture> pages = new List<Texture>();
+
+    private int curPageIndex;
+    private float curPageAngle;
+    private float totalPageAngle;
+
+    private Texture frontTex;
+    private Texture backTex;
+    private bool isFliping;
+    private Material mat;
+
+
+    #endregion
+
+
+    #region 方法
+
+    public void Start()
+    {
+        StartCoroutine(Gravity());
+        this.mat = this.GetComponent<MeshRenderer>().sharedMaterial;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        this.isFliping = true;
+        Debug.Log("true");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.totalPageAngle = this.totalPageAngle - eventData.delta.x * 0.005f;
+        this.curPageIndex = (int)Mathf.Floor(this.totalPageAngle);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        this.isFliping = false;
+        Debug.Log("false");
+    }
+
+
+	private IEnumerator Gravity()
+    {
+		while (this.isFliping) {
+			yield return null;
+		}
+
+		while (!this.isFliping) {
+			int targetPageIndex = this.totalPageAngle - this.curPageIndex > 0.5f ? this.curPageIndex + 1 : this.curPageIndex;
+			this.totalPageAngle = Mathf.Lerp (this.totalPageAngle, targetPageIndex, 0.05f);
+
+			if (Mathf.Abs (this.totalPageAngle - targetPageIndex) < 0.001f)
+            {
+				this.totalPageAngle = targetPageIndex;
+                this.curPageIndex = targetPageIndex;
+				break;
+			}
+			yield return null;
+		}
+		this.isFliping = true;
+
+		yield return null;
+
+		StartCoroutine (Gravity());
+	}
+
+
+    #endregion
+
+}
---
+++ Assets/Shader/FlipBookShader.shader
@@ -0,0 +1,109 @@
+﻿//
+// created by jiadong chen
+// http://www.chenjd.me
+//
+
+Shader "chenjd/FlipBookShader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_BackTex ("BackPage", 2D) = "white" {}
+		_CurPageAngle ("CurPageAngle", Range(0,1)) = 0 
+	}
+
+	SubShader
+	{
+			
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		CGINCLUDE
+
+		#include "UnityCG.cginc"
+		#define pi 3.1415926
+
+		sampler2D _MainTex;
+		float4 _MainTex_ST;
+
+		sampler2D _BackTex;
+		float4 _BackTex_ST;
+
+		float _CurPageAngle;
+
+		struct appdata
+		{
+			float4 vertex : POSITION;
+			float2 uv : TEXCOORD0;
+		};
+
+		struct v2f
+		{
+			float2 uv : TEXCOORD0;
+			UNITY_FOG_COORDS(1)
+			float4 vertex : SV_POSITION;
+		};
+
+
+		float4 flip_book(float4 vertex)
+		{
+			float theta = _CurPageAngle * pi;
+
+			float4 temp = vertex;
+
+			temp.x = vertex.x * cos(theta);
+			temp.y = vertex.x * sin(theta);
+
+			vertex = temp;
+
+			return vertex;
+		}
+
+		v2f vert_flip (appdata v)
+		{
+			v2f o;
+			o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+			o.uv.xy = 1 - o.uv.xy;
+
+			float4 vertex = o.uv.x <= 0.5 ? v.vertex : flip_book(v.vertex);
+			o.vertex = UnityObjectToClipPos(vertex);
+
+			return o;
+		}
+		
+		fixed4 frag_flip (v2f i) : SV_Target
+		{
+			// sample the texture
+			fixed4 col = tex2D(_MainTex, i.uv);
+			// apply fog
+			UNITY_APPLY_FOG(i.fogCoord, col);
+			return col;
+		}
+
+		ENDCG
+
+
+		//用3个pass来实现翻书的效果
+		//第一页
+		Pass
+		{
+			Cull Back
+			CGPROGRAM
+			#pragma vertex vert_flip
+			#pragma fragment frag_flip
+			// make fog work
+			#pragma multi_compile_fog
+
+
+			
+			ENDCG
+		}
+
+		//翻起来的背面
+		//pass{}
+
+		//第二页
+		//pass{}
+
+	}
+}
